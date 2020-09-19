556d6829d85f285a32256441b695ef5b4750cda7
slactjohnson <tjohnson@slac.stanford.edu>
2020-09-19 07:47:45 -0700

    Push my hacks from MEC's network/NFS/??? issues last night.

--- experiments/lv8018.py
+++ experiments/lv8018.py
@@ -130,7 +130,7 @@ def lpl_save_master_timing():
 class User():
     thz_blocked_pos = 10.0 
     thz_passed_pos = 50.0  
-    spl_blocked_pos = 5.5 
+    spl_blocked_pos = 5.94
     spl_passed_pos = 2.5 
     thz_motor = thz_motor
     spl_motor = spl_motor
@@ -152,7 +152,18 @@ class User():
     def lpl_save_master_timing(self):
         lpl_save_master_timing()
 
-    def longpulse_shot(self, record=True, end_run=True):
+    def open_shutters(self):
+        print("Opening shutters...")
+        for shutter in self.shutters:
+            self._shutters[shutter].open()
+
+    def close_shutters(self):
+        print("Closing shutters...")
+        for shutter in self.shutters:
+            self._shutters[shutter].close()
+
+#    def longpulse_shot(self, record=True, end_run=True):
+    def longpulse_shot(self, record=True):
         """
         Returns a BlueSky plan to perform a long pulse laser shot. Collects a
         long pulse laser only shot.
@@ -162,12 +173,13 @@ class User():
         record : bool <default: True>
             Flag to record the data (or not).
 
-        end_run : bool <default: True>
-            Flag to end the run after completion (or not).
         """
+#        end_run : bool <default: True>
+#            Flag to end the run after completion (or not).
+
         logging.debug("Calling User.longpulse_shot with parameters:")        
         logging.debug("record: {}".format(record))        
-        logging.debug("end_run: {}".format(end_run))
+#        logging.debug("end_run: {}".format(end_run))
 
         print("Closing shutters...")
         for shutter in self.shutters:
@@ -182,7 +194,8 @@ class User():
         yield from bps.mv(self.spl_motor, self.spl_blocked_pos, wait=True)
 
         print("Configuring DAQ...")
-        daq.configure(events=1, record=record)
+#        daq.configure(events=1, record=record)
+        daq.configure(record=record)
 
         print("Configuring sequencer...")
         # Setup the pulse picker for single shots in flip flop mode
@@ -193,20 +206,25 @@ class User():
         seq.play_mode.put(0) # Run sequence once
         # Setup sequence
         self._seq.rate = 10
-        s = self._seq.opticalSequence(1, 'longpulse')
+#        s = self._seq.opticalSequence(1, 'longpulse')
+        s = self._seq.duringSequence(1, 'longpulse')
         seq.sequence.put_seq(s)
 
-        yield from count([daq, seq], num=1)
+        print("Now run 'daq.begin_infinite()' and press 'start' on the",
+              "sequencer.")
+#        yield from count([daq, seq], num=1)
+#
+#        if end_run:
+#            time.sleep(3)
+#            daq.end_run()
+#
+#
+#        print("Opening shutters...")
+#        for shutter in self.shutters:
+#            self._shutters[shutter].open()
 
-        if end_run:
-            daq.end_run()
-
-
-        print("Opening shutters...")
-        for shutter in self.shutters:
-            self._shutters[shutter].open()
-
-    def xrd_cal(self, nshots=1, record=True, end_run=True):
+#    def xrd_cal(self, nshots=1, record=True, end_run=True):
+    def xrd_cal(self, nshots=1, record=True):
         """
         Returns a BlueSky plan to perform an XRD calibration run. Collects a
         number of X-ray only shots.
@@ -219,13 +237,13 @@ class User():
         record : bool <default: True>
             Flag to record the data (or not).
 
-        end_run : bool <default: True>
-            Flag to end the run after completion (or not).
         """
+#        end_run : bool <default: True>
+#            Flag to end the run after completion (or not).
         logging.debug("Calling User.xrd_cal with parameters:")        
         logging.debug("nshots: {}".format(nshots))        
         logging.debug("record: {}".format(record))        
-        logging.debug("end_run: {}".format(end_run))
+#        logging.debug("end_run: {}".format(end_run))
 
 
         print("Closing shutters...")
@@ -240,8 +258,9 @@ class User():
         print("Blocking Short Pulse...")
         yield from bps.mv(self.spl_motor, self.spl_blocked_pos, wait=True)
 
-        print("Configuring DAQ...")
-        daq.configure(events=nshots, record=record)
+#        print("Configuring DAQ...")
+#        daq.configure(events=nshots, record=record)
+        daq.configure(record=record)
 
         print("Configuring sequencer...")
         # Setup the pulse picker for single shots in flip flop mode
@@ -256,17 +275,21 @@ class User():
         s = self._seq.darkXraySequence(1)
         seq.sequence.put_seq(s)
 
-        yield from count([daq, seq], num=1)
+        print("Now run 'daq.begin_infinite()' and press 'start' on the",
+              "sequencer.")
+#        yield from count([daq, seq], num=1)
+#
+#        if end_run:
+#            time.sleep(3)
+#            daq.end_run()
 
-        if end_run:
-            daq.end_run()
 
+#        print("Opening shutters...")
+#        for shutter in self.shutters:
+#            self._shutters[shutter].open()
 
-        print("Opening shutters...")
-        for shutter in self.shutters:
-            self._shutters[shutter].open()
-
-    def ech_background(self, nshots=1, record=True, end_run=True):
+#    def ech_background(self, nshots=1, record=True, end_run=True):
+    def ech_background(self, nshots=1, record=True):
         """
         Returns a BlueSky plan to perform an echelon background run. Collects a 
         number of short pulse shots with THz generation blocked. 
@@ -279,13 +302,14 @@ class User():
         record : bool <default: True>
             Flag to record the data (or not).
 
-        end_run : bool <default: True>
-            Flag to end the run after completion (or not).
         """
+#        end_run : bool <default: True>
+#            Flag to end the run after completion (or not).
+
         logging.debug("Calling User.ech_background with parameters:")        
         logging.debug("nshots: {}".format(nshots))        
         logging.debug("record: {}".format(record))        
-        logging.debug("end_run: {}".format(end_run))
+#        logging.debug("end_run: {}".format(end_run))
 
         print("Closing shutters...")
         for shutter in self.shutters:
@@ -299,8 +323,9 @@ class User():
         print("Un-Blocking Short Pulse...")
         yield from bps.mv(self.spl_motor, self.spl_passed_pos, wait=True)
 
-        print("Configuring DAQ...")
-        daq.configure(events=nshots, record=record)
+#        print("Configuring DAQ...")
+#        daq.configure(events=nshots, record=record)
+        daq.configure(record=record)
 
         print("Configuring sequencer...")
         # Setup the pulse picker for single shots in flip flop mode
@@ -315,16 +340,20 @@ class User():
         s = self._seq.opticalSequence(1, 'shortpulse')
         seq.sequence.put_seq(s)
 
-        yield from count([daq, seq], num=1)
+        print("Now run 'daq.begin_infinite()' and press 'start' on the",
+              "sequencer.")
+#        yield from count([daq, seq], num=1)
+#
+#        if end_run:
+#            time.sleep(3)
+#            daq.end_run()
 
-        if end_run:
-            daq.end_run()
+#        print("Opening shutters...")
+#        for shutter in self.shutters:
+#            self._shutters[shutter].open()
 
-        print("Opening shutters...")
-        for shutter in self.shutters:
-            self._shutters[shutter].open()
-
-    def thz_reference(self, nshots=1, record=True, end_run=True):
+#    def thz_reference(self, nshots=1, record=True, end_run=True):
+    def thz_reference(self, nshots=1, record=True):
         """
         Returns a BlueSky plan to perform an THz reference run. Collects a 
         number of short pulse laser shots with THz generation un-blocked. 
@@ -336,14 +365,14 @@ class User():
 
         record : bool <default: True>
             Flag to record the data (or not).
-
-        end_run : bool <default: True>
-            Flag to end the run after completion (or not).
         """
+#        end_run : bool <default: True>
+#            Flag to end the run after completion (or not).
+
         logging.debug("Calling User.thz_reference with parameters:")        
         logging.debug("nshots: {}".format(nshots))        
         logging.debug("record: {}".format(record))        
-        logging.debug("end_run: {}".format(end_run))
+#        logging.debug("end_run: {}".format(end_run))
 
         # Un-Block THz generation
         print("Un-Blocking THz generation...")
@@ -354,7 +383,8 @@ class User():
         yield from bps.mv(self.spl_motor, self.spl_passed_pos, wait=True)
 
         print("Configuring DAQ...")
-        daq.configure(events=nshots, record=record)
+#        daq.configure(events=nshots, record=record)
+        daq.configure(record=record)
 
         print("Configuring sequencer...")
         # Setup the pulse picker for single shots in flip flop mode
@@ -369,16 +399,20 @@ class User():
         s = self._seq.opticalSequence(1, 'shortpulse')
         seq.sequence.put_seq(s)
 
-        yield from count([daq, seq], num=1)
+        print("Now run 'daq.begin_infinite()' and press 'start' on the",
+              "sequencer.")
+#        yield from count([daq, seq], num=1)
+#
+#        if end_run:
+#            time.sleep(3)
+#            daq.end_run()
+#
+#        print("Opening shutters...")
+#        for shutter in self.shutters:
+#            self._shutters[shutter].open()
 
-        if end_run:
-            daq.end_run()
-
-        print("Opening shutters...")
-        for shutter in self.shutters:
-            self._shutters[shutter].open()
-
-    def thz_drive(self, record=True, end_run=True):
+#    def thz_drive(self, record=True, end_run=True):
+    def thz_drive(self, record=True):
         """
         Returns a BlueSky plan to perform a shot with the short pulse, THz
         generation, and long pulse drive, with the XFEL. Takes a single shot.  
@@ -388,12 +422,12 @@ class User():
         record : bool <default: True>
             Flag to record the data (or not).
 
-        end_run : bool <default: True>
-            Flag to end the run after completion (or not).
         """
+#        end_run : bool <default: True>
+#            Flag to end the run after completion (or not).
         logging.debug("Calling User.thz_drive with parameters:")        
         logging.debug("record: {}".format(record))        
-        logging.debug("end_run: {}".format(end_run))
+        #logging.debug("end_run: {}".format(end_run))
 
         # Un-Block THz generation
         print("Un-Blocking THz generation...")
@@ -401,16 +435,15 @@ class User():
 
         # Un-Block SPL
         print("Un-Blocking Short Pulse...")
-        yield from bps.mv(self.spl_motor, self.spl_passed_pos, wait=True)
-
         print("Configuring DAQ...")
-        daq.configure(events=1, record=record)
+        #daq.configure(events=1, record=record)
+        daq.configure(record=record)
 
         print("Configuring sequencer...")
         # Setup the pulse picker for single shots in flip flop mode
         pp.flipflop(wait=True)
         # Setup sequencer for requested rate
-        sync_mark = int(self._sync_markers[5])
+        sync_mark = int(self._sync_markers[10])
         seq.sync_marker.put(sync_mark)
         seq.play_mode.put(0) # Run once
         # Setup sequence
@@ -418,11 +451,14 @@ class User():
         s = self._seq.dualDuringSequence()
         seq.sequence.put_seq(s)
 
-        yield from count([daq, seq], num=1)
+        print("Now run 'daq.begin_infinite()' and press 'start' on the",
+              "sequencer.")
+        #yield from count([daq, seq], num=1)
 
-        if end_run:
-            daq.end_run()
+        #if end_run:
+        #    time.sleep(3)
+        #    daq.end_run()
 
-        print("Opening shutters...")
-        for shutter in self.shutters:
-            self._shutters[shutter].open()
+        #print("Opening shutters...")
+        #for shutter in self.shutters:
+        #    self._shutters[shutter].open()
